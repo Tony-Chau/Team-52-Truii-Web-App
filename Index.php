3506abec514fee207661929e0961ceee89e02945
Marcus Chan <sakuya1814@gmail.com>
2017-08-08 19:35:00 +1000

    Bootstrap implemented

---
+++ Index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <title>Truii</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+</head>
+<body>
+
+  <button class="btn btn=success" onclick="$(this).hide();"> Click me! </button>
+  <div class="container"><!--container-fluid-->
+      <div class="row">
+          <div class="col-lg-1 col-xs-4 bg-warning"><!--bootstrap starts using the smallest first-->
+              1
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-info">
+              2
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-danger">
+              3
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-success">
+              4
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-warning">
+              5
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-info">
+              6
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-danger">
+              7
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-success">
+              8
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-warning">
+              9
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-info">
+              10
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-danger">
+              11
+          </div>
+          <div class="col-lg-1 col-xs-4 bg-success">
+              12
+          </div>
+          <!--Only 12 columns are allowed... want to see what happens when you have a 13th column?
+          <div class="col-lg-1 col-xs-4 bg-success">
+              13
+          </div>-->
+      </div>
+  </div>
+  <!-- JavaScript files should be linked at the bottom of the page  -->
+  <script src="js/jquery.min.js"></script>
+
+  <!-- Latest compiled and minified JavaScript  -->
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
