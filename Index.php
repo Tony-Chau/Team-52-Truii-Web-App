15ad2e0cfbd0c7e29e4e707951db592f33ec1127
Marcus Chan <sakuya1814@gmail.com>
2017-08-10 18:58:19 +1000

    Fixed the Container for the container of the login and the registration

--- Index.php
+++ Index.php
@@ -2,50 +2,40 @@
 <html>
 <head>
 
- <title>Truii</title>
- <link rel="stylesheet" href="css/bootstrap.min.css">
- <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <title>Truii</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
 <body style="background-image: url('BackGround/StartBackGround.png');background-position:right top;">
 
-  <img src="Logo/truii-full-colour-white.png" alt="TruiiLogo" style="width:50%;height:15%;margin-top:5%;margin-left:25%;margin-right:25%";>
+  <img src="Logo/truii-full-colour-white.png" class="logo" alt="TruiiLogo">
 
-  <div class="container-fluid"
-      style="width:90%;margin-top:1%;margin-left:5%;margin-right:5%;background:rgba(253, 103, 26, 0.8);border-radius:25px;">
+  <div class="container-fluid">
 
         <form action="action.php">
-          <div class="row" align="center" style="width:90%;margin-top:10%;margin-left:5%;margin-right:5%;">
-            <div class="div-username">
-              <label style="display:inline-block;width:30%;font-size:150%;color:#FFFFFF;">
-                Username
-              </label><br>
-                <input type="text" name="username" style="width:100%;border-radius:5px;padding-left:2%;font-size:150%;">
-            </div>
+          <div class="row-top">
+            <label class="label">Username</label><br>
+            <input type="text" name="username" class="input">
           </div>
 
-          <div class="row" align="center" style="width:90%;margin-top:5%;margin-left:5%;margin-right:5%;">
-            <div class="div-password">
-              <label style="display:inline-block;width:30%;font-size:150%;color:#FFFFFF;">
-                Password
-              </label><br>
-                <input type="password" name="password" style="width:100%;border-radius:5px;padding-left:2%;font-size:150%;">
-            </div>
+          <div class="row-mid">
+            <label class="label">Password</label><br>
+            <input type="password" name="password" class="input">
           </div>
 
-          <div class="row" align="center" style="width:90%;margin-top:10%;margin-left:5%;margin-right:5%;">
-            <div class="div-submit">
-              <input type="submit" value="Login"style="width:100%;border-radius:5px;font-size:150%;">
-            </div>
+          <div class="row-button">
+            <input type="submit" value="Login" class="submit">
           </div>
         </form>
 
-          <div class="row" align="center" style="width:90%;margin-top:10%;margin-left:5%;margin-right:5%; margin-bottom:10%;">
-            <div class="div-registration">
-              <form action="Registration.php">
-                <input type="submit" value="Register" style="width:100%;border-radius:5px;font-size:150%;"/>
-              </form>
-            </div>
+          <div class="row-bottom">
+            <form action="Registration.php">
+              <input type="submit" value="Register" class="submit">
+            </form>
           </div>
   </div>
   <!-- JavaScript files should be linked at the bottom of the page  -->
--- Registration.php
+++ Registration.php
@@ -2,50 +2,38 @@
 <html>
 <head>
 
- <title>Truii</title>
- <link rel="stylesheet" href="css/bootstrap.min.css">
- <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <title>Truii</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
 <body style="background-image: url('BackGround/StartBackGround.png');background-position:right top;">
 
-  <img src="Logo/truii-full-colour-white.png" alt="TruiiLogo" style="width:50%;height:15%;margin-top:5%;margin-left:25%;margin-right:25%";>
+  <img src="Logo/truii-full-colour-white.png" class="logo" alt="TruiiLogo">
 
-  <div class="container-fluid"
-      style="width:90%;margin-top:1%;margin-left:5%;margin-right:5%;background:rgba(253, 103, 26, 0.8);border-radius:25px;">
+  <div class="container-fluid">
 
         <form action="action.php">
-          <div class="row" align="center" style="width:90%;margin-top:10%;margin-left:5%;margin-right:5%;">
-            <div class="div-email">
-              <label style="display:inline-block;width:30%;font-size:150%;color:#FFFFFF;">
-                Email
-              </label><br>
-                <input type="text" name="email" style="width:100%;border-radius:5px;padding-left:2%;font-size:150%;">
-            </div>
+          <div class="row-top">
+            <label class="label">Email</label><br>
+            <input type="text" name="email" class="input">
           </div>
 
-          <div class="row" align="center" style="width:90%;margin-top:5%;margin-left:5%;margin-right:5%;">
-            <div class="div-password">
-              <label style="display:inline-block;width:30%;font-size:150%;color:#FFFFFF;">
-                Password
-              </label><br>
-                <input type="password" name="password" style="width:100%;border-radius:5px;padding-left:2%;font-size:150%;">
-            </div>
+          <div class="row-mid">
+            <label class="label">Password</label><br>
+            <input type="password" name="password" class="input">
           </div>
 
-          <div class="row" align="center" style="width:90%;margin-top:5%;margin-left:5%;margin-right:5%;">
-            <div class="div-confirmpassword">
-              <label style="display:inline-block;width:60%;font-size:150%;color:#FFFFFF;">
-                Confirm Password
-              </label><br>
-                <input type="password" name="confirmpassword" style="width:100%;border-radius:5px;padding-left:2%;font-size:150%;">
-            </div>
+          <div class="row-mid">
+            <label class="label">Confirm Password</label><br>
+            <input type="password" name="confirmpassword" class="input">
           </div>
 
-          <div class="row" align="center" style="width:90%;margin-top:10%;margin-left:5%;margin-right:5%;margin-bottom:10%;">
-            <div class="div-submit">
-              <input type="submit" value="Register"style="width:100%;border-radius:5px;font-size:150%;">
-            </div>
+          <div class="row-bottom">
+            <input type="submit" value="Register" class="submit">
           </div>
         </form>
   </div>
