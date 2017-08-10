7ca0e58cd53d3b02feff69c324ac421be25ca2c6
Tony Chau <tonychau923@gmail.com>
2017-08-10 17:40:39 +1000

    Convert the php files to html and Cookie function has been created (note: The mysql part may be used as backup until we hear more news about the Truii API)

---
+++ js/Functions/Cookies.js
@@ -0,0 +1,26 @@
+function WriteCookie(name, value, expirytime){
+  var date = new Date();
+  var expiry = expirytime*24*60*60*1000; // will last for 24 hours if expiry time = 1
+  date.setTime(date.getTime() + (expiry);
+  var expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function readCookie(Cookiename){
+  var name = Cookiename + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var cookieArray = decodedCookie.split(';');
+  for (var i = 0; i < cookieArray.length; i += 1){
+    var cookie = cookieArray[i];
+    while(cookie.charAt(0) == ' '){
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if (cookie.indexOf(name) == 0){
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+}
+
+function CheckCookie(name){
+  return (readCookie(name) != '');
+}
