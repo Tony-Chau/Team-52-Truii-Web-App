4fa68ad81f556c298d6d1d1aaf9ab15755a1d9d5
Tony Chau <tonychau923@gmail.com>
2017-08-10 21:27:49 +1000

    Fixed up the GetLink function and made a javascript version

---
+++ js/Functions/Link.js
@@ -0,0 +1,19 @@
+function GetLink(PageDirectory, Type, Value){
+  var link = "" . PageDirectory;
+  if (Type === null || Value === null || Type == "" || Value == ""){
+    return link;
+  }
+  var length = Type.length;
+  link += "?";
+  if (length > 1){
+    for (var i = 0; i < length; i += 1){
+      link += str.replace(' ', '%20', Type[i]) + "=" + str.replace(' ', '_', Value[i]);
+      if (i != length - 1){
+        link += "&";
+      }
+    }
+  }else if(length == 1){
+    link += str.replace(' ', '%20', Type) + "=" + str.replace(' ', '_', Value);
+  }
+  return link;
+}
