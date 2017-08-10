bcefd86fdc61378ea6d243f07a53bf32dd27e941
Marcus Chan <sakuya1814@gmail.com>
2017-08-10 19:02:38 +1000

    php copy of the html

---
+++ Index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Truii</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body style="background-image: url('BackGround/StartBackGround.png');background-position:right top;">
+
+  <img src="Logo/truii-full-colour-white.png" class="logo" alt="TruiiLogo">
+
+  <div class="container-fluid">
+
+        <form action="action.php">
+          <div class="row-top">
+            <label class="label">Username</label><br>
+            <input type="text" name="username" class="input">
+          </div>
+
+          <div class="row-mid">
+            <label class="label">Password</label><br>
+            <input type="password" name="password" class="input">
+          </div>
+
+          <div class="row-button">
+            <input type="submit" value="Login" class="submit">
+          </div>
+        </form>
+
+          <div class="row-bottom">
+            <form action="Registration.php">
+              <input type="submit" value="Register" class="submit">
+            </form>
+          </div>
+  </div>
+  <!-- JavaScript files should be linked at the bottom of the page  -->
+  <script src="js/jquery.min.js"></script>
+
+  <!-- Latest compiled and minified JavaScript  -->
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ Registration.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Truii</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body style="background-image: url('BackGround/StartBackGround.png');background-position:right top;">
+
+  <img src="Logo/truii-full-colour-white.png" class="logo" alt="TruiiLogo">
+
+  <div class="container-fluid">
+
+        <form action="action.php">
+          <div class="row-top">
+            <label class="label">Email</label><br>
+            <input type="text" name="email" class="input">
+          </div>
+
+          <div class="row-mid">
+            <label class="label">Password</label><br>
+            <input type="password" name="password" class="input">
+          </div>
+
+          <div class="row-mid">
+            <label class="label">Confirm Password</label><br>
+            <input type="password" name="confirmpassword" class="input">
+          </div>
+
+          <div class="row-bottom">
+            <input type="submit" value="Register" class="submit">
+          </div>
+        </form>
+  </div>
+  <!-- JavaScript files should be linked at the bottom of the page  -->
+  <script src="js/jquery.min.js"></script>
+
+  <!-- Latest compiled and minified JavaScript  -->
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
