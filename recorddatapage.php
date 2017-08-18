be541453227499e4b27c25df66d47412cf67b019
Meeralallu <meera.lallu@connect.qut.edu.au>
2017-08-18 13:35:28 +1000

    Record the data page
    HTML of the record data page

---
+++ recorddatapage.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <title>Truii Record Page</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+ <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+
+<header id ="titlelogo">
+  <div class="container">
+  <span class="logo"></span>
+    <h1>
+      Record Data
+    </h1>
+  </div>
+
+
+</header>
+
+<div id ="recordform">
+  <div class= "container1">
+
+
+  <form>
+    <div class="form-group">
+      <label for="exampletextinput">Title </label>
+      <input type="text" class="form-control" id="xampletextinput" aria-describedby="tablename" placeholder="Enter Title">
+
+    </div>
+
+    <div class="form-group">
+      <label for="exampleSelect1">Number of columns</label>
+      <select class="form-control" id="exampleSelect1">
+
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+
+<h2> Column 1 </h2>
+  <div class="form-group">
+    <label for="exampletextinput">Column 1 Title </label>
+    <input type="text" class="form-control" id="xampletextinput" aria-describedby="tablename" placeholder="Enter Title">
+
+  </div>
+
+  <div class="form-group">
+    <label for="exampleSelect1">Unit of Measurement</label>
+    <select class="form-control" id="exampleSelect1">
+      <option>% Percentage</option>
+      <option># Numbers</option>
+      <option> Text</option>
+    </select>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
