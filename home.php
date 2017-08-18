b345b046c21c110d636782e466b8e189223f09fe
Meeralallu <meera.lallu@connect.qut.edu.au>
2017-08-18 15:16:27 +1000

    Home Page
    HTML of the home page

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <title>Truii Home</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+ <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<header id ="titlelogo1">
+  <div class="container1">
+  <span class="logo1"></span>
+
+  </div>
+
+</header>
+
+
+<div id ="Homebutton">
+  <div class= "container1">
+
+<form name="myform" action="graph.php" method="POST">
+<div align="center">
+<br><input type="image" src="images//graphmarker.png" name="image" width="120" height="80"><br>
+<h1 style="color:#EF6724;"> Graph Marker </h1>
+</div>
+
+<div>
+  <form name="myform" action="recorddatapage.php" method="POST">
+  <div align="center">
+  <br><input type="image" src="images//recorddata.png" name="image" width="120" height="80"><br>
+  <h1 style="color:#0ABFDD;"> Record Data</h1>
+</div>
+</form>
+</body>
