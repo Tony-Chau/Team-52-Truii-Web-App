0f18ce2ba34da31cbafc167ea78e3ba719ea93c4
Tony Chau <tonychau923@gmail.com>
2017-08-22 14:55:46 +1000

    Database is now connected with the login and registration page (email function has not been implemented within the site yet due to the email confirmation page has yet been created

--- Index.php
+++ Index.php
@@ -1,11 +1,48 @@
+<?php
+  include './sql/mysql.inc';
+  //unset($_SESSION['UserID']);
+  if (is_log()){
+    header('location: home.php');
+  }
+  $log_username = "";
+  $reg_username = "";
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //Log
+      if (isset($_POST['log_username'])){
+          if(CheckUser($_POST['log_username'])){
+            if (CheckUserAndPassword($_POST['log_username'], $_POST['log_password'])){
+              LogUser($_POST['log_username'], $_POST['log_password']);
+              header('location: Index.php');
+            }
+          }
+          echo "<script>alert('Sorry but the login details you have entered do not match within our database, please retype your password');</script>";
+          $log_username = $_POST['log_username'];
+      }
+      //Register
+      else{
+        if (isset($_POST['reg_username'])){
+          if ($_POST['reg_password'] == $_POST['reg_confirmpassword']){
+            if (!CheckUser($_POST['reg_username'])){
+              CreateUser($_POST['reg_username'], $_POST['reg_confirmpassword']);
+              LogUser($_POST['reg_username'], $_POST['reg_password']);
+              header('location: Index.php');
+            }
+              echo "<script>alert('Sorry, but this email has already been registered. Please use a different email account');</script>";
+          }
+          else{
+              echo "<script>alert('Sorry, but your passwords do not match. Please try again');</script>";
+          }
+          $reg_username = $_POST['reg_username'];
+        }
+      }
+  }
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
-
   <title>Truii</title>
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/bootstrap-theme.css">
-
   <link rel="stylesheet" href="css/main.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -19,16 +56,16 @@
   <div class="centered" align="center">
     <div class="container-fluid">
 
-        <form action="action.php">
+        <form method=POST>
           <div id = "log">
           <div class="row-top">
             <label class="label">Username</label><br>
-            <input type="text" name="username" class="input">
+            <?php echo "<input type=\"email\" name=\"log_username\" class=\"input\" value=\"" . $log_username . "\" required>" ?>
           </div>
 
           <div class="row-mid">
             <label class="label">Password</label><br>
-            <input type="password" name="password" class="input">
+            <input type="password" name="log_password" class="input" required>
           </div>
 
           <div class="row-button">
@@ -37,24 +74,24 @@
         </form>
 
           <div class="row-bottom">
-              <input type="button" value="Register" class="submit"  id= "btnRegister"></input>
+              <input type="button" value="Register" class="submit"  id= "btnRegister">
           </div>
         </div>
-          <form action="action.php" style="display: none;" id = "Register">
+          <form method=POST style="display: none;" id="Register">
             <div id ="Register">
             <div class="row-top" >
               <label class="label">Email</label><br>
-              <input type="email" name="email" class="input" required>
+              <?php echo "<input type=\"email\" name=\"reg_username\" class=\"input\" value=\"" . $reg_username . "\" required>"; ?>
             </div>
 
             <div class="row-mid">
               <label class="label">Password</label><br>
-              <input type="password" name="password" class="input">
+              <input type="password" name="reg_password" class="input">
             </div>
 
             <div class="row-mid">
               <label class="label">Confirm Password</label><br>
-              <input type="password" name="confirmpassword" class="input" required>
+              <input type="password" name="reg_confirmpassword" class="input" required>
             </div>
 
             <div class="row-bottom">
@@ -65,7 +102,6 @@
             </div>
           </div>
           </form>
-        <!--onclick="window.location='Registration.php';"-->
     </div>
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -78,11 +114,10 @@
         $("#btnLog").click(function(){
         $("#Register").animate({ height: 'toggle' ,opacity: '0'}, "slow");
           $("#log").animate({height: 'toggle' , opacity: '100' }, "slow");
-
-        });
+        }),
+        $('[name=log_username]').value = 'pa';
     });
 </script>
-
   <!-- JavaScript files should be linked at the bottom of the page  -->
   <script src="js/jquery.min.js"></script>
 
