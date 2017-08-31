af39ddf37ff49f1889cb02e6e62e455406140a09
mizmeera <meera.lallu@connect.qut.edu.au>
2017-08-31 18:09:23 +1000

    Choose Data page (HTML and CSS)
    Using Bootstrap to create a list

---
+++ choosedatabase.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <title>Data Library</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+ <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<header id ="titlelogo2">
+  <div class="container">
+  <h1> Data Library </h1>
+
+  </div>
+
+</header>
+
+<div id ="Homebutton">
+  <div class= "container">
+
+    <center><img src="images/truii-full-colour-white.png" alt="Truii" width="200" height="128"></center>
+
+
+    <div class="list-group">
+      <a href="#" class="list-group-item active">
+        Data 1
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">Data 2</a>
+      <a href="#" class="list-group-item list-group-item-action">Data 3</a>
+      <a href="#" class="list-group-item list-group-item-action">Data 4</a>
+      <a href="#" class="list-group-item list-group-item-action">Data 5</a>
+    </div>
