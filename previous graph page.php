f7dda41d6446dade1d479e5063b708c08736eecf
mizmeera <meera.lallu@connect.qut.edu.au>
2017-08-31 17:14:23 +1000

    Chart library page
    This page allows user to view their previous charts

---
+++ previous graph page.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <title>Chart Library</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+ <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<header id ="titlelogo2">
+  <div class="container">
+  <h1> Chart Library </h1>
+
+  </div>
+
+</header>
+
+
+<div id ="Homebutton">
+  <div class= "container">
+
+
+    <div class="row">
+      <div class="col-sm-12 col-md-4">
+        <div class="thumbnail">
+          <img src="images/pies.png" alt="...">
+          <div class="caption">
+            <h3>Chart 1</h3>
+            <p>Year 11's results</p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-12 col-md-4">
+        <div class="thumbnail">
+          <img src="images/bars.png" alt="...">
+          <div class="caption">
+            <h3>Chart 2</h3>
+            <p>Exam 1 results</p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+          </div>
+        </div>
+      </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="thumbnail">
+            <img src="..." alt="...">
+            <div class="caption">
+              <h3>Chart 3</h3>
+              <p>Year 9's results</p>
+              <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+            </div>
+          </div>
+    </div>
+
+  </div>
+
+  <div class="row">
+    <div class="col-sm-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="...">
+        <div class="caption">
+          <h3>Chart 4</h3>
+          <p> Number of students that passed</p>
+          <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="...">
+        <div class="caption">
+          <h3>Chart 5</h3>
+          <p>Year 8s Results</p>
+          <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+        </div>
+      </div>
+    </div>
+      <div class="col-sm-12 col-md-4">
+        <div class="thumbnail">
+          <img src="..." alt="...">
+          <div class="caption">
+            <h3>Chart 6</h3>
+            <p>Exam 4 Results</p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+          </div>
+        </div>
+  </div>
+
+</div>
+</div>
+</div>
+</html>
