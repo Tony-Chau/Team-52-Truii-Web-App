5fcede99338b414084495ce2285f75a3ec34f666
Marcus Chan <sakuya1814@gmail.com>
2017-09-06 21:14:02 +1000

    Connected, the 'chartmaker' to the 'chart type' page as well as the 'previous graph page'. created and connected two charts to the 'previous graph page' as stand in for the current chart library the 'two' charts are currently static so current unable to edit data in real time

--- chartmaker.php
+++ chartmaker.php
@@ -8,30 +8,32 @@
  <link rel="stylesheet" href="css/style.css">
 
 </head>
-
+<body>
 <header id ="titlelogo2">
   <div class="container">
-  <h1> Chart Maker </h1>
-
+    <h1> Chart Maker </h1>
   </div>
-
 </header>
 
-<div id ="Homebutton">
-  <div class= "container">
+  <div id ="Homebutton">
+    <div class= "container">
 
-<form name="myform" action="chartmaker.php" method="POST">
-<div align="center">
-<br><input type="image" src="images//addnew.png" name="image" width="180" height="120"><br>
-<h1 style="color:#EF6724;"> New Chart </h1>
-</div>
+      <form name="myform" action="charttype.php" method="POST">
+        <div align="center">
+          <br><input type="image" src="images//addnew.png" name="image" width="180" height="120"><br>
+          <h1 style="color:#EF6724;"> New Chart </h1>
+        </div>
+      </form>
 
-<div>
-  <form name="myform" action="recorddatapage.php" method="POST">
-  <div align="center">
-  <br><input type="image" src="images//viewold.png" name="image" width="180" height="120"><br>
-  <h1 style="color:#EF6724;"> View Previous Charts</h1>
-</div>
-</form>
 
+      <form name="myform" action="previous graph page.php" method="POST">
+        <div align="center">
+          <br><input type="image" src="images//viewold.png" name="image" width="180" height="120"><br>
+          <h1 style="color:#EF6724;"> View Previous Charts</h1>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</body>
 </html>
--- previous graph page.php
+++ previous graph page.php
@@ -8,42 +8,42 @@
  <link rel="stylesheet" href="css/style.css">
 
 </head>
-
+<body>
 <header id ="titlelogo2">
   <div class="container">
-  <h1> Chart Library </h1>
-
+    <h1> Chart Library </h1>
   </div>
-
 </header>
 
 
-<div id ="Homebutton">
-  <div class= "container">
+  <div id ="Homebutton">
+    <div class= "container">
 
 
-    <div class="row">
-      <div class="col-sm-12 col-md-4">
-        <div class="thumbnail">
-          <img src="images/pies.png" alt="...">
-          <div class="caption">
-            <h3>Chart 1</h3>
-            <p>Year 11's results</p>
-            <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+      <div class="row">
+
+        <div class="col-sm-12 col-md-4">
+          <div class="thumbnail">
+            <img src="images/pies.png" alt="...">
+            <div class="caption">
+              <h3>Chart 1</h3>
+              <p>Year 11's results</p>
+              <p><a href="Graphs/year11_results.php" class="btn btn-primary" role="button">View</a> </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-sm-12 col-md-4">
-        <div class="thumbnail">
-          <img src="images/bars.png" alt="...">
-          <div class="caption">
-            <h3>Chart 2</h3>
-            <p>Exam 1 results</p>
-            <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+        <div class="col-sm-12 col-md-4">
+          <div class="thumbnail">
+            <img src="images/bars.png" alt="...">
+            <div class="caption">
+              <h3>Chart 2</h3>
+              <p>Exam 1 results</p>
+              <p><a href="Graphs/exam1_results.php" class="btn btn-primary" role="button">View</a> </p>
+            </div>
           </div>
         </div>
-      </div>
+
         <div class="col-sm-12 col-md-4">
           <div class="thumbnail">
             <img src="..." alt="...">
@@ -53,44 +53,48 @@
               <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
-    </div>
-
-  </div>
-
-  <div class="row">
-    <div class="col-sm-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="...">
-        <div class="caption">
-          <h3>Chart 4</h3>
-          <p> Number of students that passed</p>
-          <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
         </div>
-      </div>
-    </div>
 
-    <div class="col-sm-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="...">
-        <div class="caption">
-          <h3>Chart 5</h3>
-          <p>Year 8s Results</p>
-          <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
-        </div>
       </div>
-    </div>
-      <div class="col-sm-12 col-md-4">
-        <div class="thumbnail">
-          <img src="..." alt="...">
-          <div class="caption">
-            <h3>Chart 6</h3>
-            <p>Exam 4 Results</p>
-            <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+      <div class="row">
+
+        <div class="col-sm-12 col-md-4">
+          <div class="thumbnail">
+            <img src="..." alt="...">
+            <div class="caption">
+              <h3>Chart 4</h3>
+              <p> Number of students that passed</p>
+              <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+            </div>
           </div>
         </div>
+
+        <div class="col-sm-12 col-md-4">
+          <div class="thumbnail">
+            <img src="..." alt="...">
+            <div class="caption">
+              <h3>Chart 5</h3>
+              <p>Year 8s Results</p>
+              <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+          <div class="thumbnail">
+            <img src="..." alt="...">
+            <div class="caption">
+              <h3>Chart 6</h3>
+              <p>Exam 4 Results</p>
+              <p><a href="#" class="btn btn-primary" role="button">View</a> </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   </div>
 
-</div>
-</div>
-</div>
+</body>
 </html>
