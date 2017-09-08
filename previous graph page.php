7a3bd706347ca63c60afee2623194d3e41c239f1
Marcus Chan <sakuya1814@gmail.com>
2017-09-09 01:10:40 +1000

    Labeled all the chart pages file name with their respective charts name

--- previous graph page.php
+++ previous graph page.php
@@ -28,7 +28,7 @@
             <div class="caption">
               <h3>Chart 1</h3>
               <p>Year 11's results</p>
-              <p><a href="Graphs/year11_results.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/year11_results(Pie Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -39,7 +39,7 @@
             <div class="caption">
               <h3>Chart 2</h3>
               <p>Exam results</p>
-              <p><a href="Graphs/exam_results.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/exam_results(Bar Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -50,7 +50,7 @@
             <div class="caption">
               <h3>Chart 3</h3>
               <p>Year 9's results</p>
-              <p><a href="Graphs/year9_results.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/year9_results(Horizontal Bar Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -64,7 +64,7 @@
             <div class="caption">
               <h3>Chart 4</h3>
               <p>Student Overall Results During HighSchool</p>
-              <p><a href="Graphs/results_overtime.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/results_overtime(Scatter+Line Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -75,7 +75,7 @@
             <div class="caption">
               <h3>Chart 5</h3>
               <p>Year 8s Results</p>
-              <p><a href="Graphs/year8_results.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/year8_results(Overlaid Area Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -86,7 +86,7 @@
             <div class="caption">
               <h3>Chart 6</h3>
               <p>Exam Average Results</p>
-              <p><a href="Graphs/exam_average_results.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/exam_average_results(Bubble Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -100,7 +100,7 @@
             <div class="caption">
               <h3>Chart 7</h3>
               <p>Number of Cheating Students Caught During a Year</p>
-              <p><a href="Graphs/cheatingstudents.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/cheatingstudents(Scatter Plot Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -111,7 +111,7 @@
             <div class="caption">
               <h3>Chart 8</h3>
               <p>Number of Sick Students During a Year</p>
-              <p><a href="Graphs/sickstudents.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/sickstudents(Line Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
@@ -122,7 +122,7 @@
             <div class="caption">
               <h3>Chart 9</h3>
               <p>Classes Exam Average Results</p>
-              <p><a href="Graphs/exam_average_class_results.php" class="btn btn-primary" role="button">View</a> </p>
+              <p><a href="Graphs/exam_average_class_results(Line Dash Chart).php" class="btn btn-primary" role="button">View</a> </p>
             </div>
           </div>
         </div>
