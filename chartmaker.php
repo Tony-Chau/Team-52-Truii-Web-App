b647dd9a9bd10c7717e33a5fe678df8729ec9053
mizmeera <meera.lallu@connect.qut.edu.au>
2017-09-10 20:48:51 +1000

    Updated the top bar and fixed buttons
    Added the Home button and back button the top bar, and made the button bigger

--- chartmaker.php
+++ chartmaker.php
@@ -18,22 +18,40 @@
 <body>
 <header id ="titlelogo2">
   <div class="container">
+    <div class="row">
+      <div class="col-xs-6">
     <h1> Chart Maker </h1>
-  </div>
-</header>
+    </div>
+      <div class="col-xs-3">
 
+      <div class="icon">
+      <img class="" src="images//homeicon-01.png" alt=""> </div>
+
+      </div>
+
+  <div class="col-xs-3">
+  <div class="icon">
+      <img class="" src="images//back-01.png" alt=""> </div>
+</div>
+</div>
+</div>
+
+</header>
+  
   <div id ="Homebutton">
     <div class= "container">
 
           <a href="charttype.php" style="text-decoration:none">
         <div align="center">
-          <br><input type="image" src="images//addnew.png" name="image" width="180" height="120"><br>
+            <div class="big1">
+              <br><input type="image" src="images//addnew.png" name="image" ><br></div>
           <h1 style="color:#EF6724;"> New Chart </h1>
         </div></a>
 
           <a href="charttype.php" style="text-decoration:none">
         <div align="center">
-          <br><input type="image" src="images//viewold.png" name="image" width="180" height="120"><br>
+          <div class="big2">
+            <br><input type="image" src="images//viewold.png" name="image"><br></div>
           <h1 style="color:#EF6724;"> View Previous Charts</h1>
         </div></a>
 
