f4ac7b6e728951cb0ca763d52367df885b645400
Tony Chau <tonychau923@gmail.com>
2017-10-06 22:32:15 +1000

    improvised the code to allow users to select which chart they want to make

--- datapageXY.php
+++ datapageXY.php
@@ -67,21 +67,35 @@ include 'sql/Bootgrid/getcolumns.php';
             <div class="container" id="XYSelector">
 
               <br />
-
               <div class="input-group">
-                <select name="x_column_selected" id="X_column_selected1" onChange="Xdataselected('1');" class="form-control">
+              <select name="Chart" id="ChartOption" onChange="ChartSelected();" class="form-control">
+                  <option>Please select a chart</option>
+                  <option>Scatter plot</option>
+                  <option>Line Dash</option>
+                  <option>Bubble</option>
+                  <option>Bar</option>
+                  <option>Scatter Line</option>
+                  <option>Line</option>
+                  <option>Overlaid Area</option>
+                  <option>Horizontal Bar</option>
+                  <option>Pie</option>
+                </select>
+              </div>
+              <br/>
+              <div class="input-group">
+                <select name="x_column_selected" id="X_column_selected1" onChange="Xdataselected('1');" disabled class="form-control">
                   <option value=0>Select X Value</option>
                   <?php echo $outputx ?>
                 </select>
                 <span class="input-group-btn">
-                  <button class="btn btn-default" type="button"> X </button>
+                  <button class="btn btn-default" type="button" id='buttonx' disabled> X </button>
                 </span>
-                <select name="Y_column_selected" id="Y_column_selected1" onChange="Ydataselected();" class="form-control">
+                <select name="Y_column_selected" id="Y_column_selected1" onChange="Ydataselected();" disabled class="form-control">
                   <option value=0>Select Y Value</option>
                   <?php echo $outputy ?>
                 </select>
                 <span class="input-group-btn">
-                  <button class="btn btn-default" type="button"> Y </button>
+                  <button class="btn btn-default" type="button" id='buttony' disabled> Y </button>
                 </span>
               </div>
               <br />
@@ -136,37 +150,78 @@ include 'sql/Bootgrid/getcolumns.php';
 </html>
 <script>
 var size = Number("<?php echo $size;?>");
+var chart = '';
 var x_select = [size];
-for (var i = 1; i < size; i += 1){
+var options = [size];
+for (var i = 0; i < size; i += 1){
   x_select[i] = '';
+}
+"<?php for ($i = 1; $i < $size; $i += 1){?>";
+  options[Number("<?php echo $i;?>")] = "<?php echo $arr['rows'][$i]['FieldName']?>";
+"<?php } ?>";
+function ChartSelected(){
+  var select = document.getElementById('ChartOption');
+  if (select.selectedIndex != 0){
+    chart = select.options[select.selectedIndex].text;
+    document.getElementById('X_column_selected1').disabled = false;
+    document.getElementById('Y_column_selected1').disabled = false;
+    document.getElementById('buttonx').disabled = false;
+    document.getElementById('buttony').disabled = false;
+    EnabledOrDisableOption('x', false);
+    EnabledOrDisableOption('y', false);
+  }else{
+    chart = '';
+    document.getElementById('X_column_selected1').disabled = true;
+    document.getElementById('Y_column_selected1').disabled = true;
+    document.getElementById('buttonx').disabled = true;
+    document.getElementById('buttony').disabled = true;
+    EnabledOrDisableOption('x', true);
+    EnabledOrDisableOption('y', true);
+  }
 }
 
 function Xdataselected(num){
   var select = document.getElementById('X_column_selected'+num);
-  x_select[num] = '';
-  for (var i = 1; i < size; i += 1){
-    var option = select.options;
-    document.getElementById('y-' + option[i].text).disabled = false;
-  }
-  for (var i = 1; i < size; i += 1){
-    if (x_select[i] != ''){
-      document.getElementById(x_select[i]).disabled = true;
-    }
-  }
   var index = select.selectedIndex;
   var selectedOption = select.options;
   var Value = selectedOption[index].text;
-  document.getElementById('y-' + Value).disabled = true;
-  x_select[num] = 'y-' + Value;
+
+  if (index == 0){
+    document.getElementById(x_select[num]).disabled = false;
+  }else{
+    x_select[num] = '';
+    for (var i = 1; i < size; i += 1){
+      var option = selectedOption;
+      document.getElementById('y-' + option[i].text).disabled = false;
+    }
+    for (var i = 1; i < size; i += 1){
+      if (x_select[i] != ''){
+        document.getElementById(x_select[i]).disabled = true;
+      }
+    }
+    document.getElementById('y-' + Value).disabled = true;
+    x_select[num] = 'y-' + Value;
+  }
+
 }
 
 function Ydataselected(){
   var select = document.getElementById('Y_column_selected1');
-  for (var i = 1; i < size; i += 1){
-    document.getElementById('x-' + select.options[i].text).disabled = false;
-  }
   var index = select.selectedIndex;
-  document.getElementById('x-' + select.options[index].text).disabled = true;
+  var selectedOption = select.options;
+  var Value = selectedOption[index].text;
+  for (var i = 1; i < size; i += 1){
+    EnabledOrDisableOption('x', false);
+  }
+  if (index != 0){
+    document.getElementById('x-' + selectedOption[index].text).disabled = true;
+  }
+}
+
+function EnabledOrDisableOption(axis, bool){
+  for (var i = 1; i < size; i += 1){
+    document.getElementById(axis+ '-' + options[i]).disabled = bool;
+  }
 }
 </script>
 
