50a1e2e57ddafd7f5e27552abf389dbd80bb169e
Marcus Chan <sakuya1814@gmail.com>
2017-10-19 21:19:01 +1000

    did more fixes to the home page

--- home.php
+++ home.php
@@ -25,24 +25,23 @@
       <div class="row">
         <div class="col">
 
-          <div align="center">
-              <a href='previousgraphpage.php' style="text-decoration:none">
-                <input type="image" src="images//graphmarker.png" name="image" style="width: 75%; height: 30%"><br>
-                <h1 style="color:#EF6724;" style='margin-left:5%;'> Chart Marker </h1>
-              </a>
+          <div align="center" style="margin-top: 20%">
+            <a href='previousgraphpage.php' style="text-decoration:none;">
+              <input type="image" src="images//graphmarker.png" name="image" style="width: 75%; height: 30%"><br>
+              <h1 style="color:#EF6724;" style='margin-left:5%;'> <b style="font-size: 150%;">Chart Marker</b> </h1>
+            </a>
           </div>
-          <br unselectable="on">
 
         </div>
       </div>
       <div class="row">
         <div class="col">
 
-          <div align="center">
-               <a href='recorddatapageAddDelete.php' style="text-decoration:none">
-                 <br><input type="image" src="images//recorddata.png" name="image" style="width: 75%; height: 30%"><br>
-                 <h1 style="color:#0ABFDD;" style='margin-left:5%;'> Record Data</h1>
-               </a>
+          <div align="center" style="font-size: 200%;">
+            <a href='recorddatapageAddDelete.php' style="text-decoration:none;">
+              <br><input type="image" src="images//recorddata.png" name="image" style="width: 75%; height: 30%"><br>
+              <h1 style="color:#0ABFDD;" style='margin-left:5%;'> <b style="font-size: 150%;">Record Data</b> </h1>
+            </a>
           </div>
 
         </div>
