28364d9f0d016a75b9ca3203e196ba9195ea996f
Marcus Chan <sakuya1814@gmail.com>
2017-10-23 22:00:33 +1000

    did design changes to the settings page

--- settings.php
+++ settings.php
@@ -20,46 +20,48 @@
 <body>
 
   <div id ="Homebutton">
-  <h1>
-    My Account
-    <h1/>
-    <form>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Name</label>
-        <div class="col-sm-10">
-          <p class="form-control-static">Jane Doe</p>
+    <div class="container" style="padding: 0; margin: 0;">
+      <label style="font-size: 125%; margin-top: 6%; margin-bottom: 5%">My Account</label>
+      <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 3%">
+        <label>Name</label>
+        <div class="col-sm-10" style="margin-top:-3%;">
+          <h5>Jane Doe</h5>
         </div>
       </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-          <p class="form-control-static">email@example.com</p>
+
+      <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 3%">
+        <label>Email</label>
+        <div class="col-sm-10" style="margin-top:-3%; ">
+          <h5>email@example.com</h5>
         </div>
-        <button type="button" class="btn btn-primary1" id="btnfadeToItemColumn">Change Password</button>
       </div>
-      <div class="form-group row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Current Password</label>
-        <div class="col-sm-10">
+
+      <div class="row" style="padding: 0; padding-left:10px; padding-right:50px; margin-bottom: 3%">
+        <label for="inputPassword">Current Password</label>
+        <div class="col">
           <input type="password" class="form-control" id="inputPassword" placeholder="Password">
         </div>
+      </div>
 
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">New Password</label>
-          <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-          </div>
+      <div class="row" style="padding: 0; padding-left:10px; padding-right:50px; margin-bottom: 5%">
+        <label for="inputPassword">New Password</label>
+        <div class="col">
+          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+          <span style="color: red">Password does not match</span>
         </div>
+        <button type="button" class="btn btn-primary1" id="btnfadeToItemColumn" style="margin-top: 5%" >Change Password</button>
+      </div>
 
-        <h1>
-          Get Help
-          <h1>
-            <a href='#' onclick='mail();'><p> contact Truii</p></a>
+      <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 10%">
+        <label>Get Help</label>
+        <div class="col-sm-10">
+          <a href='#' onclick='mail();'><label style="font-size: 90%"> Contact Truii</label></a><br/>
+        </div>
+      </div>
 
-          </div>
-        <h1>
-          Logout
-        </h1>
-        <button type="button" class="btn btn-primary" id="btnfadeToItemColumn">Logout</button>
-
-
-    </form>
+      <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 3%">
+        <form method="POST">
+          <button type="button" class="btn btn-primary" id="btnfadeToItemColumn">Logout</button>
+        </form>
+      </div>
+    </div>
