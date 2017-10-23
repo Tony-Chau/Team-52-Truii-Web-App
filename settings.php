afcc3e94c5150af6a190c7f4527300abfc06e43e
Tony Chau <tonychau923@gmail.com>
2017-10-23 22:43:41 +1000

    You can now logout with the settings page You can now change your password in the settings page

--- settings.php
+++ settings.php
@@ -7,6 +7,24 @@
     gotoPage('Index');
   }
   CheckRequestLogout();
+  $UserDetail = GetAllUserDetail($_SESSION['UserID']);
+  $name = '';
+  $email = '';
+  $password = '';
+  foreach ($UserDetail as $data){
+    $name = $data['Name'];
+    $email = $data['Username'];
+    $password = $data['Password'];
+  }
+  if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['ChangePassword'])){
+    if ($_POST['password'] == $password){
+      ChangeUserPassword($_POST['con_password']);
+      CallTestAlert('Password has been changed');
+    }else{
+      CallTestAlert('Your current password do not match');
+    }
+  }
+
   navBarCreate('rgb(252, 103, 25)', 'settings');
 ?>
 <!DOCTYPE html>
@@ -25,33 +43,34 @@
       <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 3%">
         <label>Name</label>
         <div class="col-sm-10" style="margin-top:-3%;">
-          <h5>Jane Doe</h5>
+          <h5><?php echo $name; ?></h5>
         </div>
       </div>
 
       <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 3%">
         <label>Email</label>
         <div class="col-sm-10" style="margin-top:-3%; ">
-          <h5>email@example.com</h5>
+          <h5><?php echo $email ?></h5>
         </div>
       </div>
 
       <div class="row" style="padding: 0; padding-left:10px; padding-right:50px; margin-bottom: 3%">
         <label for="inputPassword">Current Password</label>
+        <form method='POST'>
         <div class="col">
-          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+          <input type="password" class="form-control" id="password" placeholder="Password" name='password'>
         </div>
       </div>
 
       <div class="row" style="padding: 0; padding-left:10px; padding-right:50px; margin-bottom: 5%">
         <label for="inputPassword">New Password</label>
         <div class="col">
-          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-          <span style="color: red">Password does not match</span>
+          <input type="password" class="form-control" id="con_password" name='con_password' placeholder="Password">
+          <span style="color: red" id='SpanMessage'></span>
         </div>
-        <button type="button" class="btn btn-primary1" id="btnfadeToItemColumn" style="margin-top: 5%" >Change Password</button>
+        <input type="submit" class="btn btn-primary1" id="btnChange" style="margin-top: 5%" name='ChangePassword'>
       </div>
-
+    </form>
       <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 10%">
         <label>Get Help</label>
         <div class="col-sm-10">
@@ -61,7 +80,7 @@
 
       <div class="row" style="padding: 0; padding-left:10px; margin-bottom: 3%">
         <form method="POST">
-          <button type="button" class="btn btn-primary" id="btnfadeToItemColumn">Logout</button>
+          <input type="submit" name='logout' class="btn btn-primary" value='Logout'>
         </form>
       </div>
     </div>
